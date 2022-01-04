fa3316f82cbdff19b8cb4974fb4d4c961601339f
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2022-01-04 12:41:57 -0400

    handle errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-
-const products =[
-  {
-    "id": 1,
-    "category": "shoes",
-    "image": "shoe1.jpg",
-    "name": "Hiker",
-    "price": 94.95,
-    "skus": [
-      { "sku": "17", "size": 7 },
-      { "sku": "18", "size": 8 }
-    ],
-    "description": "This rugged boot will get you up the mountain safely."
-  },
-  {
-    "id": 2,
-    "category": "shoes",
-    "image": "shoe2.jpg",
-    "name": "Climber",
-    "price": 78.99,
-    "skus": [
-      { "sku": "28", "size": 8 },
-      { "sku": "29", "size": 9 }
-    ],
-    "description": "Sure-footed traction in slippery conditions."
-  },
-  {
-    "id": 3,
-    "category": "shoes",
-    "image": "shoe3.jpg",
-    "name": "Explorer",
-    "price": 145.95,
-    "skus": [
-      { "sku": "37", "size": 7 },
-      { "sku": "38", "size": 8 },
-      { "sku": "39", "size": 9 }
-    ],
-    "description": "Look stylish while stomping in the mud."
-  }
-]
+import { getProducts } from "./services/productService.js";
 
 export default function App() {
+  const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const [size, setSize] = useState('')
+  useEffect(() => {
+    getProducts("shoes")
+      .then((res) => setProducts(res))
+      .catch((e) => setError(e));
+  }, []);
 
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
+  const renderProduct = (p) => (
+    <div key={p.id} className="product">
+      <a href="/">
+        <img src={`/images/${p.image}`} alt={p.name} />
+        <h3>{p.name}</h3>
+        <p>${p.price}</p>
+      </a>
+    </div>
+  );
 
- const filterProducts = size ? products.filter(product => product.skus.find(s => s.size === parseInt(size))) : products
+  const filterProducts = size
+    ? products.filter((product) =>
+        product.skus.find((s) => s.size === parseInt(size))
+      )
+    : products;
+
+  if (error) throw error;
 
   return (
     <>
@@ -68,7 +41,11 @@ export default function App() {
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size" value={size} onChange={e => setSize(e.target.value)}>
+            <select
+              id="size"
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
@@ -76,9 +53,7 @@ export default function App() {
             </select>
             {size && <h2>Found {filterProducts.length} items</h2>}
           </section>
-          <section id='products'>
-            {filterProducts.map(renderProduct)}
-          </section>
+          <section id="products">{filterProducts.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
