a1614d7b16b40192ed4a69aded1d66dbe985b911
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2022-01-05 21:22:46 -0400

    Add routes and params to main page and Products

---
+++ src/Products.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
+import { useParams } from "react-router-dom";
+
+export default function App() {
+  const [size, setSize] = useState("");
+  const { category } = useParams();
+
+  const {
+    data: products,
+    error,
+    loading,
+  } = useFetch("products?category=" + category);
+
+  const renderProduct = (p) => (
+    <div key={p.id} className="product">
+      <a href="/">
+        <img src={`/images/${p.image}`} alt={p.name} />
+        <h3>{p.name}</h3>
+        <p>${p.price}</p>
+      </a>
+    </div>
+  );
+
+  const filterProducts = size
+    ? products.filter((product) =>
+        product.skus.find((s) => s.size === parseInt(size))
+      )
+    : products;
+
+  if (error) throw error;
+  if (loading) return <Spinner />;
+
+  return (
+    <>
+      <section id="filters">
+        <label htmlFor="size">Filter by Size:</label>{" "}
+        <select
+          id="size"
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+        >
+          <option value="">All sizes</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
+        {size && <h2>Found {filterProducts.length} items</h2>}
+      </section>
+      <section id="products">{filterProducts.map(renderProduct)}</section>
+    </>
+  );
+}
