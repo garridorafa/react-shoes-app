4814cc28a3836b8c42f90615f2931f654e78bcd9
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2022-01-11 16:34:19 -0400

    Add isMountedRef to api calls

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,29 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+  const isMountedRef = useRef(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    isMountedRef.current = true;
     const init = async () => {
       try {
         const res = await fetch(baseUrl + url);
         if (res.ok) {
           const json = await res.json();
-          setData(json);
+          if (isMountedRef.current) setData(json);
         } else {
           throw res;
         }
       } catch (e) {
-        setError(e);
+        if (isMountedRef.current) setError(e);
       } finally {
-        setLoading(false);
+        if (isMountedRef.current) setLoading(false);
       }
     };
     init();
+
+    return () => {
+      isMountedRef.current = false;
+    };
   }, [url]);
 
   return { data, error, loading };
