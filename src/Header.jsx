4c6f5a11ba5658fa439bcafa3aa68ebc0b6fe57f
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2022-01-11 16:34:35 -0400

    Fix structure bug in jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,16 +14,16 @@ export default function Header() {
             <Link to="/">
               <img alt="Carved Rock Fitness" src="/images/logo.png" />
             </Link>
-            <li>
-              <NavLink activeStyle={activeStyle} to="/shoes">
-                Shoes
-              </NavLink>
-            </li>
-            <li>
-              <NavLink activeStyle={activeStyle} to="/cart">
-                Cart
-              </NavLink>
-            </li>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to="/shoes">
+              Shoes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to="/cart">
+              Cart
+            </NavLink>
           </li>
         </ul>
       </nav>
