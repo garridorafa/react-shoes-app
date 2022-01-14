f7bd5bee9307d7322fe8e7540c45a5ef9f8aa46a
Rafael Garrido <rafaelgarrido@tecnologiateradata15.com>
2022-01-13 20:56:12 -0400

    Refactor CartProvider

--- src/cartContext.js
+++ src/cartContext.js
@@ -1,3 +1,24 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import cartReducer from "./cartReducer";
 
 export const CartContext = React.createContext(null);
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
+}
+
+export function CartProvider({ children }) {
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+  const contextValue = {
+    cart,
+    dispatch,
+  };
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+}
